4d7980c68247597bd49d459a450b956f8438fad9
Vlad <oneviddi@gmail.com>
2020-05-24 11:32:09 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.981ba8a67d347b3b27b4e2a262ddabe6.js"
+  "./precache-manifest.0177920e0a430f055da2b7d4bedb11db.js"
 );
 
 self.addEventListener('message', (event) => {
